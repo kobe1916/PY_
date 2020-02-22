11a480ee788d53f969eb7e04751fd17e9645eadf
kobe1916 <47083794+kobe1916@users.noreply.github.com>
2020-02-22 21:54:45 +0800

    Create 书本信息.py

---
+++ Data Analysis/Pandas/书本信息.py
@@ -0,0 +1,31 @@
+# coding = utf-8
+import pandas as pd
+from matplotlib import pyplot as plt
+
+file_path = "./books.csv"
+
+df = pd.read_csv(file_path)
+
+
+
+#取出有效信息
+data1= df[pd.notnull(df["original_publication_year"])]
+
+grouped = data1.groupby(by="original_publication_year").count()["title"]
+
+#不同年份书的平均评分情况
+#去除original_publication_year列中nan的行
+data1 = df[pd.notnull(df["original_publication_year"])]
+
+grouped =data1["average_rating"].groupby(by=data1["original_publication_year"]).mean()
+print(grouped)
+
+_x = grouped.index
+_y = grouped.values
+
+#画图
+plt.figure(figsize=(20,8),dpi=80)
+plt.plot(range(len(_x)),_y)
+
+plt.xticks(list(range(len(_x)))[::100],_x[::100],rotation=90)
+plt.show()
