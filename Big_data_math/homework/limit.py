1823f1b8c298271a342ffc4db26eeab2a60cc058
kobe1916 <47083794+kobe1916@users.noreply.github.com>
2020-03-09 15:48:03 +0800

    Create limit.py

---
+++ Big_data_math/homework/limit.py
@@ -0,0 +1,16 @@
+from sympy import*
+x = Symbol('x')
+y = (x*83-x**2+x/2)**exp(1/x)-sqrt(x**6+1)
+limit(y,x,oo)
+
+x = Symbol('x')
+y = sin(x)**tan(x)
+limit(y,x,pi/2,dir='-')
+
+x = Symbol('x')
+y = ((4*x+3)/(4*x+1))**(2*x+5)
+limit(y,x,oo)
+
+x = Symbol('x')
+y = ((1-1/2*x**2)**(2/3)-1)/x*log(1+x)
+limit(y,x,0)
