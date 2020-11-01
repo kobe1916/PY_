bbe2a64abba3b8217146d77463a501a7bdcf4aa3
kobe1916 <47083794+kobe1916@users.noreply.github.com>
2020-11-01 15:35:17 +0800

    Update test.py

--- Data Analysis/DA/批量修改文件名/test.py
+++ Data Analysis/DA/批量修改文件名/test.py
@@ -8,7 +8,41 @@ print(os.name)
 os.mkdir('./文件夹')
 #相对路径
 #新建文件夹  .当前目录
+'''
+C:\Users\lhy\Desktop\Data analysis
+nt
+'''
 
-//C:\Users\lhy\Desktop\Data analysis
-//nt
+import os
+#返回绝对路径
+result = os.path.abspath('文件夹')
+#print(result)
 
+#判断是否时文件夹   isdir
+result = os.path.isdir('文件夹')
+#print(result)
+
+#判断是否是文件   isfile
+result = os.path.isfile('./批量修改文件名.ipynb')
+#print(result)
+
+#
+result = os.path.splitext('./批量修改文件名.ipynb')
+#print(result)
+
+#判断文件是否存在
+result = os.path.exists('C:/Users/lhy/Desktop/Data analysis/批量修改文件名.ipynb')
+#print(result)
+
+#创建时间   ctime时间戳
+result = os.path.getctime('C:/Users/lhy/Desktop/Data analysis/批量修改文件名.ipynb')
+print(result)
+
+import time
+time_now = time.localtime(result)
+mytime = time.strftime('%Y-%m-%d %H:%H:%S',time_now)
+print(mytime)
+'''
+1603782432.2329137
+2020-10-27 15:15:12
+'''
