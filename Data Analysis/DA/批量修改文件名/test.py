9ec29ff5e8f1ba447536fe979e58ac93bb901948
kobe1916 <47083794+kobe1916@users.noreply.github.com>
2020-11-01 15:33:58 +0800

    Create test.py

---
+++ Data Analysis/DA/批量修改文件名/test.py
@@ -0,0 +1,14 @@
+##  OS模块
+
+import os
+print(os.getcwd())
+#当前文件路径
+print(os.name)
+#当前系统   posix  Linux/unix系统   nt  Windows系统
+os.mkdir('./文件夹')
+#相对路径
+#新建文件夹  .当前目录
+
+//C:\Users\lhy\Desktop\Data analysis
+//nt
+
