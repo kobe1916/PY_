6a450cff40062a4a54bbe103382dec2e0434d2fa
kobe1916 <47083794+kobe1916@users.noreply.github.com>
2020-11-03 19:49:10 +0800

    Update re模块.py

--- Data Analysis/DA/批量修改文件名/re模块.py
+++ Data Analysis/DA/批量修改文件名/re模块.py
@@ -4,4 +4,16 @@ result = re.findall("正则表示匹配规则",
                    "利用(正则表示匹配规则)到这个字符串查找符合规则的数据")
 print(f'根据正则匹配结果:{result}')
 
-#findaii(规则，箱子)
+#findall(规则，箱子)
+
+
+import re 
+result = re.findall("正则表示匹配规则",
+                   "利用(正则表示匹配规则)到这个字符串查找符合规则的数据正则表示匹配规则")
+print(f'根据正则匹配结果:{result}')
+
+#匹配电话号
+str1 = '我的电话号： 13888888888，他的电话号：13666666666'
+#result = re.findall('\d\d\d\d\d\d\d\d\d\d\d',str1)
+result = re.findall('\d{11}',str1)
+print(result)
